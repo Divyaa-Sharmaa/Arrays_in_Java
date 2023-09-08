7d6697a872c658e75ad3093697ce2759625fefbb
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-09 01:57:16 +0530

    Max SubArray Sum

--- maxSubArray.java
+++ maxSubArray.java
@@ -8,7 +8,8 @@ public class maxSubArray {
         int num = sc.nextInt();
         int[] arr = new int[num];
         inputArr(arr);
-        maxSubArraySum(arr);
+        // maxSubArraySum(arr);
+        prefixSum(arr);
     }
 
     public static void inputArr(int[] arr){
@@ -32,6 +33,25 @@ public class maxSubArray {
             }
         } 
         System.out.println("count is: " + maxSum);
+    }
+
+    // by Prefix Sum
+    public static void prefixSum(int[] arr){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0]=arr[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1] +arr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currSum = i==0?prefix[j]:prefix[j]-prefix[i-1];
+                if(maxSum<currSum){
+                    maxSum = currSum;
+                }
+            }
+        }System.out.println("max sum is: "  +maxSum);
     }
     
 }
