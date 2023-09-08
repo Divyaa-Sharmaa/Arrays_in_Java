7e5363ca50e240c0d3706595059494cddd559977
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-09 01:14:06 +0530

    SubArray with 2 approaches

--- subArray.java
+++ subArray.java
@@ -8,7 +8,8 @@ public class subArray {
         int num = sc.nextInt();
         int[] arr = new int[num];
         inputArr(arr);
-        subArrays(arr);
+        // subArrays(arr);
+        subArraysAnothrApp(arr);
     }
 
     public static void inputArr(int[] arr){
@@ -29,5 +30,18 @@ public class subArray {
             System.out.println();
         }
     }
-    
+     // or we can do this with another approach 
+
+     public static void subArraysAnothrApp(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+     }
+
 }
