a7ee46af47d0c8f88023606e82bc9f2a1c67bc5c
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-08 23:20:48 +0530

    Pair with nuumbers

---
+++ pairNum.java
@@ -0,0 +1,32 @@
+package Arrays_in_Java;
+
+import java.util.Scanner;
+
+public class pairNum {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        inputArr(arr);
+        pairWithNum(arr);
+    }
+
+    public static void inputArr(int[] arr){
+        for (int i = 0; i< arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void pairWithNum(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if(i==j){
+                    continue;
+                }
+                System.out.print("(" + arr[i] +"," +arr[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+    
+}
