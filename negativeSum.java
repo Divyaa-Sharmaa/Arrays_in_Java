ea83e270d3707504f1123b265dcc4a933c941143
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-09 11:41:32 +0530

    SubArray with negative sum

--- negativeSum.java
+++ negativeSum.java
@@ -8,7 +8,8 @@ public class negativeSum {
         int num = sc.nextInt();
         int[] arr = new int[num];
         inputArr(arr);
-        subArrNegaSum(arr);
+        // subArrNegaSum(arr);
+        prefixSum(arr);
     }
 
     public static void inputArr(int[] arr){
@@ -31,5 +32,24 @@ public class negativeSum {
             }
         }System.out.println("count is: " + count);
     }
-    
+
+    //prefixSum (optimized approach)
+    public static void prefixSum(int[] arr){
+        int currSum = 0;
+        int count = 0;
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        for(int i=0; i<arr.length;i++){
+            for (int j = i; j < prefix.length; j++) {
+                currSum = i==0?prefix[j]:prefix[j]-prefix[i-1];
+                if(currSum<0)
+                  count++;
+            }
+        }
+        System.out.println("count is: " + count);
+    }
 }
