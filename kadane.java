35f8c88b720a168602a13acf60d6475aec76f344
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-11 20:13:29 +0530

    Max SubArray

--- kadane.java
+++ kadane.java
@@ -8,7 +8,8 @@ public class kadane {
         int num = sc.nextInt();
         int[] arr = new int[num];
         inputArr(arr);
-        kadanesAlgo(arr);
+        // kadanesAlgo(arr);
+        maxSubArray(arr);
     }
 
     public static void inputArr(int[] arr){
@@ -28,6 +29,17 @@ public class kadane {
             maxSum = Math.max(currSum, maxSum);
         }
         System.out.println("our max subArray sum is: " + maxSum);
+    }
+
+    public static int maxSubArray(int[] arr){
+        int currSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            currSum = Math.max(arr[i], currSum+arr[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
     }
     
 }
